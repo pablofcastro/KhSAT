13047624e9e5499a9809c2c3eb1a3b6855539e02
Valentin Cassano <68750947+vcassano@users.noreply.github.com>
2025-11-26 20:46:25 +0800

    formula generator

---
+++ Benchmark/generator/kh.py
@@ -0,0 +1,30 @@
+def khplus(n:int):
+    
+    r = ""
+
+    for i in range(n):
+        r += f"kh(p{i},p{i+1});"
+
+    return r
+
+def khminus(n:int):
+    
+    r = ""
+
+    for i in range(n):
+        r += f"~kh(q0{i},q1{i});"
+
+    return r
+
+def khsabotage(s:list[tuple[int,int]]):
+    r = ""
+
+    for i in range(len(s)):
+
+        r += f"~kh(p{s[i][0]},p{s[i][1]})"
+
+    return r
+
+def phi(n:int, m:int, s:list[tuple[int,int]]):
+    
+    return khplus(n)+khminus(m)+khsabotage(s)
---
+++ Benchmark/generator/main.py
@@ -0,0 +1,39 @@
+import sys
+import random
+import kh 
+
+if __name__ == '__main__':
+
+    argv = sys.argv[1:]
+
+    if len(argv) >= 2:
+
+
+        if argv[0] == "-r":
+
+            b = int(argv[1])
+
+            argv  = [f"{random.randint(0,b)}"]
+            argv += [f"{random.randint(0,b)}"]
+
+            for i in range(b):
+
+                argv += [f"{random.randint(0,b)}.{random.randint(0,b)}"]
+
+        fname = "f."+".".join(argv) + ".kh"
+          
+        ffile = open(fname,'w')
+
+        n = int(argv[0])
+        m = int(argv[1])
+
+        s = [tuple(map(int, s.split("."))) for s in argv[2:]]
+
+        ffile.write(f"{kh.phi(n,m,s)}")
+        
+        ffile.close()
+
+    else:
+        print(f'error: incorrect number of arguments for {argv[0]}')
+        print(f'  use: {argv[0]} -r <b> | <n> <m> [<i.j>+]')
+
